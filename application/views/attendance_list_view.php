36ddc708f45298dab650db0b9745379c503b922d
jhunlio2023 <tyroneedong21@gmail.com>
2025-06-17 06:31:32 +0800

    report

--- application/views/attendance_list_view.php
+++ application/views/attendance_list_view.php
@@ -3,202 +3,118 @@
 
 <?php include('includes/head.php'); ?>
 
+<style>
+    .table-sm td,
+    .table-sm th {
+        padding: 0.4rem !important;
+        font-size: 13px;
+        vertical-align: middle;
+    }
+
+    .table-danger td {
+        background-color: #f8d7da !important;
+    }
+
+    .card-header {
+        cursor: pointer;
+    }
+</style>
+
 <body>
 
-    <!-- Begin page -->
     <div id="wrapper">
 
-        <!-- Topbar Start -->
         <?php include('includes/top-nav-bar.php'); ?>
-        <!-- end Topbar --> <!-- ========== Left Sidebar Start ========== -->
-
-        <!-- Lef Side bar -->
         <?php include('includes/sidebar.php'); ?>
-        <!-- Left Sidebar End -->
-
-        <!-- ============================================================== -->
-        <!-- Start Page Content here -->
-        <!-- ============================================================== -->
 
         <div class="content-page">
             <div class="content">
-
-                <!-- Start Content-->
                 <div class="container-fluid">
+<!-- Page Header -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="d-flex justify-content-between align-items-center mb-2">
+            <div>
+                <h4 class="page-title mb-1">Attendance Logs</h4>
+                <div class="text-muted small">Project: <strong><?= $project[0]->projectTitle ?? '' ?></strong></div>
+            </div>
+            <a href="<?= base_url('project/project_view') ?>" class="btn btn-secondary btn-sm">
+                 Back to Projects
+            </a>
+        </div>
+        <hr class="mt-0">
+    </div>
+</div>
 
-                    <!-- start page title -->
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="page-title-box">
-                                <h4 class="page-title">
-                                    
+                    <!-- Attendance Records -->
+                    <?php if (empty($attendance_logs)): ?>
+                        <div class="alert alert-info text-center">No attendance records found.</div>
+                    <?php else: ?>
 
+                        <?php
+                        // Group logs by attendance_date
+                        $grouped_logs = [];
+                        foreach ($attendance_logs as $log) {
+                            $grouped_logs[$log->attendance_date][] = $log;
+                        }
+                        ?>
 
-                                </h4>
-                                <div class="page-title-right">
-                                    <ol class="breadcrumb p-0 m-0">
-                                        <!-- <li class="breadcrumb-item"><a href="#"><span class="badge badge-purple mb-3">Currently login to <b>SY <?php echo $this->session->userdata('sy'); ?> <?php echo $this->session->userdata('semester'); ?></span></b></a></li> -->
-                                    </ol>
+                        <?php foreach ($grouped_logs as $date => $logs): ?>
+                            <div class="card mb-2">
+                                <div class="card-header" data-toggle="collapse" data-target="#collapse<?= md5($date) ?>" aria-expanded="false">
+                                    <h5 class="mb-0">
+                                        <?= date('F d, Y', strtotime($date)) ?>
+                                        <span class="float-right"><i class="mdi mdi-chevron-down"></i></span>
+                                    </h5>
                                 </div>
-                                <div class="clearfix"></div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end page title -->
-
-                    <div class="row">
-                        <div class="col-lg-12 col-sm-6 ">
-                            <!-- Portlet card -->
-                            <div class="card">
-                                <div class="card-header bg-info py-3 text-white">
-                                    <div class="card-widgets">
-                                        <a href="javascript:;" data-toggle="reload"><i class="mdi mdi-refresh"></i></a>
-                                        <!-- <a data-toggle="collapse" href="#cardCollpase3" role="button" aria-expanded="false" aria-controls="cardCollpase2"><i class="mdi mdi-minus"></i></a> -->
-                    <a href="<?= base_url('project/project_view') ?>" class="btn btn-secondary">Back</a>
-
-                                    </div>
-                                    <h5 class="card-title mb-0 text-white">Attendance Logs <br>
-                                   <small> <?php echo $project[0]->projectTitle; ?></small>
-                                </h5>
-                                </div>
-                                
-                                <?php if (empty($attendance_logs)): ?>
-                    <div class="alert alert-info text-center">No attendance records found.</div>
-                <?php else: ?>
-
-                    <?php
-                    // Group by date
-                    $grouped_logs = [];
-                    foreach ($attendance_logs as $log) {
-                        $grouped_logs[$log->attendance_date][] = $log;
-                    }
-                    ?>
-
-                    <?php foreach ($grouped_logs as $date => $logs): ?>
-                        <div class="card mb-2 shadow-sm">
-                            <div class="card-header" style="cursor: pointer;" data-toggle="collapse" data-target="#collapse<?= md5($date) ?>" aria-expanded="false" aria-controls="collapse<?= md5($date) ?>">
-                                <h5 class="mb-0">
-                                    <?= date('F d, Y', strtotime($date)) ?>
-                                    <span class="float-right"><i class="mdi mdi-chevron-down"></i></span>
-                                </h5>
-                            </div>
-                            <div id="collapse<?= md5($date) ?>" class="collapse">
-                                <div class="card-body p-0">
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered table-hover mb-0">
-                                            <thead class="thead-light">
-                                                <tr>
-                                                    <th>Personnel</th>
-                                                    <th>Status</th>
-                                                    <th>Work Duration (hrs)</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php foreach ($logs as $log): ?>
+                                <div id="collapse<?= md5($date) ?>" class="collapse">
+                                    <div class="card-body p-0">
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered table-sm table-hover mb-0">
+                                                <thead class="thead-light">
                                                     <tr>
-                                                        <td><?= htmlspecialchars($log->first_name . ' ' . $log->last_name) ?></td>
-                                                        <td><?= htmlspecialchars($log->attendance_status) ?></td>
-                                                        <td><?= htmlspecialchars($log->workDuration) ?></td>
+                                                        <th>Personnel</th>
+                                                        <th>Status</th>
+                                                        <th>Work Duration (hrs)</th>
                                                     </tr>
-                                                <?php endforeach; ?>
-                                            </tbody>
-                                        </table>
+                                                </thead>
+                                                <tbody>
+                                                    <?php foreach ($logs as $log): ?>
+                                                        <tr class="<?= $log->attendance_status == 'Absent' ? 'table-danger' : '' ?>">
+                                                            <td><?= htmlspecialchars($log->first_name . ' ' . $log->last_name) ?></td>
+                                                            <td><?= htmlspecialchars($log->attendance_status) ?></td>
+                                                            <td><?= htmlspecialchars($log->workDuration) ?></td>
+                                                        </tr>
+                                                    <?php endforeach; ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
 
-                <?php endif; ?>
+                    <?php endif; ?>
 
-                <!-- Back Button -->
-             
+                </div> <!-- container-fluid -->
+            </div> <!-- content -->
 
-            </div>
-        </div>
-    </div>
-
-                        </div>
-                        <!-- end col -->
-                    </div>
-
-
-
-                </div>
-                <!-- end container-fluid -->
-
-            </div>
-            <!-- end content -->
-
-
-
-            <!-- Footer Start -->
             <?php include('includes/footer.php'); ?>
-            <!-- end Footer -->
-
         </div>
-
-        <!-- ============================================================== -->
-        <!-- End Page content -->
-        <!-- ============================================================== -->
-
     </div>
-    <!-- END wrapper -->
 
-
-    <!-- Right Sidebar -->
-    <!-- /Right-bar -->
-
-
-    <!-- Vendor js -->
+    <!-- JS Scripts -->
     <script src="<?= base_url(); ?>assets/js/vendor.min.js"></script>
-
-    <script src="<?= base_url(); ?>assets/libs/moment/moment.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/jquery-scrollto/jquery.scrollTo.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/sweetalert2/sweetalert2.min.js"></script>
-
-    <!-- Chat app -->
-    <script src="<?= base_url(); ?>assets/js/pages/jquery.chat.js"></script>
-
-    <!-- Todo app -->
-    <script src="<?= base_url(); ?>assets/js/pages/jquery.todo.js"></script>
-
-    <!--Morris Chart-->
-    <script src="<?= base_url(); ?>assets/libs/morris-js/morris.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/raphael/raphael.min.js"></script>
-
-    <!-- Sparkline charts -->
-    <script src="<?= base_url(); ?>assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
-
-    <!-- Dashboard init JS -->
-    <script src="<?= base_url(); ?>assets/js/pages/dashboard.init.js"></script>
-
-    <!-- App js -->
     <script src="<?= base_url(); ?>assets/js/app.min.js"></script>
-
-    <!-- Required datatable js -->
-    <script src="<?= base_url(); ?>assets/libs/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/datatables/dataTables.bootstrap4.min.js"></script>
-    <!-- Buttons examples -->
-    <script src="<?= base_url(); ?>assets/libs/datatables/dataTables.buttons.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/datatables/buttons.bootstrap4.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/jszip/jszip.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/pdfmake/pdfmake.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/pdfmake/vfs_fonts.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/datatables/buttons.html5.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/datatables/buttons.print.min.js"></script>
-
-    <!-- Responsive examples -->
-    <script src="<?= base_url(); ?>assets/libs/datatables/dataTables.responsive.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/datatables/responsive.bootstrap4.min.js"></script>
-
-    <script src="<?= base_url(); ?>assets/libs/datatables/dataTables.keyTable.min.js"></script>
-    <script src="<?= base_url(); ?>assets/libs/datatables/dataTables.select.min.js"></script>
-
-    <!-- Datatables init -->
-    <script src="<?= base_url(); ?>assets/js/pages/datatables.init.js"></script>
-
+    <script>
+        // Auto-expand latest group if needed
+        document.addEventListener("DOMContentLoaded", function () {
+            const collapseEls = document.querySelectorAll('.card .collapse');
+            if (collapseEls.length > 0) {
+                collapseEls[0].classList.add('show');
+            }
+        });
+    </script>
 </body>
 
 </html>
